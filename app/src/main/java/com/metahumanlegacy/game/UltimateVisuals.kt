5e7f0e54ceda8468db0216aca0e0b4b83afff8d2
managementtrika-pixel <management.trika@gmail.com>
2026-09-12 14:56:56 +0200

    fix: use Compose color animation API

--- app/src/main/java/com/metahumanlegacy/game/UltimateVisuals.kt
+++ app/src/main/java/com/metahumanlegacy/game/UltimateVisuals.kt
@@ -1,6 +1,6 @@
 package com.metahumanlegacy.game
 
-import androidx.compose.animation.core.animateColorAsState
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
