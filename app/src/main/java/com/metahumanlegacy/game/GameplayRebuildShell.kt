c06ba41ff3b11729a83e173b55478095a6ef3af4
managementtrika-pixel <management.trika@gmail.com>
2026-09-12 22:43:20 +0200

    fix(gameplay): use supported Compose layout scope extensions

--- app/src/main/java/com/metahumanlegacy/game/GameplayRebuildShell.kt
+++ app/src/main/java/com/metahumanlegacy/game/GameplayRebuildShell.kt
@@ -1,17 +1,7 @@
 package com.metahumanlegacy.game
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
