7c844619e6df70b4bd1efb705f5671f6bc00fc3d
managementtrika-pixel <management.trika@gmail.com>
2026-09-13 18:06:11 +0200

    Fix 4.1 shell dp import

--- app/src/main/java/com/metahumanlegacy/game/GameplayRebuildShell.kt
+++ app/src/main/java/com/metahumanlegacy/game/GameplayRebuildShell.kt
@@ -1,12 +1,13 @@
 package com.metahumanlegacy.game
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.foundation.background
+import androidx.compose.ui.unit.dp
 
 @Composable
 internal fun GameplayRebuildEarlyShell(
