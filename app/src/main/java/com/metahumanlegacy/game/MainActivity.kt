df7eb3ea3a473eb29faf193bb2f5646cbb760f32
managementtrika-pixel <management.trika@gmail.com>
2026-09-13 17:18:03 +0200

    feat: activate visual rebuild 4 globally

--- app/src/main/java/com/metahumanlegacy/game/MainActivity.kt
+++ app/src/main/java/com/metahumanlegacy/game/MainActivity.kt
@@ -7,6 +7,6 @@ import androidx.activity.compose.setContent
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { GameplayRebuildApp(applicationContext) }
+        setContent { VisualRebuild4App(applicationContext) }
     }
 }
